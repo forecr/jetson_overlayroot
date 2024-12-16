5d5717e8001929c1aaa485e0437f7f945cd59b9e
mdeniz0 <mehmet@forecr.io>
2024-12-16 13:42:15 +0300

    Update disable_overlayroot.sh & enable_overlayroot.sh with JetPack version check

--- disable_overlayroot.sh
+++ disable_overlayroot.sh
@@ -5,14 +5,32 @@ if [ "$(whoami)" != "root" ] ; then
 	exit 1
 fi
 
-if test -f "/etc/overlayroot.conf"; then
-	if ! overlayroot-chroot /bin/bash -c "echo 'overlayroot=disabled' > /etc/overlayroot.conf"; then
-		echo "ERR: could not run command in overlayrootfs, is it already disabled?"
+function disable_overlayroot_l4t_R32 {
+	if test -f "/etc/overlayroot.conf"; then
+		if ! overlayroot-chroot /bin/bash -c "echo 'overlayroot=disabled' > /etc/overlayroot.conf"; then
+			echo "ERR: could not run command in overlayrootfs, is it already disabled?"
+		else
+			echo "Overlay root file-system will be disabled on next boot."
+		fi
 	else
-		echo "Overlay root file-system will be disabled on next boot."
+		echo "/etc/overlayroot.conf not found"
 	fi
+}
+
+if [ "$(cat /etc/nv_tegra_release | grep -c '# R32 (release)')" == "1" ]; then
+	echo "JetPack-4.x based system detected"
+	disable_overlayroot_l4t_R32
+elif [ "$(cat /etc/nv_tegra_release | grep -c '# R35 (release)')" == "1" ]; then
+	echo "JetPack-5.x based system detected"
+	echo "Unsupported system"
+	exit 1
+elif [ "$(cat /etc/nv_tegra_release | grep -c '# R36 (release)')" == "1" ]; then
+	echo "JetPack-6.x based system detected"
+	echo "Unsupported system"
+	exit 1
 else
-	echo "/etc/overlayroot.conf not found"
+	echo "Could not detect the JetPack version"
+	exit 1
 fi
 
-
+echo "Done."
--- enable_overlayroot.sh
+++ enable_overlayroot.sh
@@ -5,10 +5,30 @@ if [ "$(whoami)" != "root" ] ; then
 	exit 1
 fi
 
-if test -f "/etc/overlayroot.conf"; then
-	echo 'overlayroot_cfgdisk="disabled"' > /etc/overlayroot.conf
-	echo 'overlayroot="tmpfs:swap=1,recurse=0"' >> /etc/overlayroot.conf
-	echo "Overlay root file-system will be enabled on next boot."
+function enable_overlayroot_l4t_R32 {
+	if test -f "/etc/overlayroot.conf"; then
+		echo 'overlayroot_cfgdisk="disabled"' > /etc/overlayroot.conf
+		echo 'overlayroot="tmpfs:swap=1,recurse=0"' >> /etc/overlayroot.conf
+		echo "Overlay root file-system will be enabled on next boot."
+	else
+		echo "/etc/overlayroot.conf not found"
+	fi
+}
+
+if [ "$(cat /etc/nv_tegra_release | grep -c '# R32 (release)')" == "1" ]; then
+	echo "JetPack-4.x based system detected"
+	enable_overlayroot_l4t_R32
+elif [ "$(cat /etc/nv_tegra_release | grep -c '# R35 (release)')" == "1" ]; then
+	echo "JetPack-5.x based system detected"
+	echo "Unsupported system"
+	exit 1
+elif [ "$(cat /etc/nv_tegra_release | grep -c '# R36 (release)')" == "1" ]; then
+	echo "JetPack-6.x based system detected"
+	echo "Unsupported system"
+	exit 1
 else
-	echo "/etc/overlayroot.conf not found"
+	echo "Could not detect the JetPack version"
+	exit 1
 fi
+
+echo "Done."
